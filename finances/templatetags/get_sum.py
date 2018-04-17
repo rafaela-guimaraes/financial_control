6cf8c613d28e0f3f5b03b3e17eec93f972da56bb
rafaelalgsantos <rafaelalg.santos@gmail.com>
2018-04-17 13:52:34 -0300

    Add balance to entries statement

--- finances/templatetags/get_sum.py
+++ finances/templatetags/get_sum.py
@@ -4,3 +4,7 @@ register = template.Library()
 @register.filter
 def get_sum(List, i):
     return List[int(i)].get('amount__sum')
+
+@register.filter
+def get_balance(income, expense):
+    return income - expense
